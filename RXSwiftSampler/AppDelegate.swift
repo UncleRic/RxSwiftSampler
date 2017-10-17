2af1ca732ac23cf045f8271aa1051588fa61e558
Frederick C. Lee <frederick.c.lee@gmail.com>
2017-10-17 14:20:26 -0700

    Initial Setup

---
+++ RXSwiftSampler/AppDelegate.swift
@@ -0,0 +1,14 @@
+//  AppDelegate.swift
+//  RXSwiftSampler
+//
+//  Created by Frederick C. Lee on 10/17/17.
+//  Copyright © 2017 Amourine Technologies. All rights reserved.
+// -----------------------------------------------------------------------------------------------------
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
---
+++ RXSwiftSampler/MainViewController.swift
@@ -0,0 +1,22 @@
+//  MainViewController.swift
+//  RXSwiftSampler
+//
+//  Created by Frederick C. Lee on 10/17/17.
+//  Copyright © 2017 Amourine Technologies. All rights reserved.
+// -----------------------------------------------------------------------------------------------------
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
